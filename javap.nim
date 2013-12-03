00a45f18271b2a7e103bea88702b407ad498bf4f
Michał Zieliński <michal@zielinscy.org.pl>
2013-12-03 23:37:40 +0100

    parse javap output

---
+++ javap.nim
@@ -0,0 +1,97 @@
+import osproc
+import streams
+import strutils
+import tables
+import re
+
+type
+  TClassInfo* = Tuple[name: string]
+  TThingKind* = enum
+    javaMethod,
+    javaField,
+    javaConstructor
+    javaSpecial
+  TThingInfo* = Tuple[
+    name: string,
+    kind: TThingKind,
+    isPublic: bool,
+    isProtected: bool,
+    isStatic: bool
+  ]
+
+proc maybeStripStart(a: var string, start: string): bool =
+  if a.startsWith(start):
+    a = a[start.len..(a.len-1)]
+    return true
+  else:
+    return false
+
+proc stripFirstWord(a: var string): bool =
+  let loc = a.find(" ")
+  if loc == -1:
+    return false
+  else:
+    a = a[loc+1..a.len-1]
+    return true
+
+proc rawJavap(name: string): auto =
+  echo "Running javap"
+  let p = osproc.startProcess("/usr/bin/javap", ".", ["-s", name])
+  return p.outputStream
+
+proc parseClassInfo(line: string): TClassInfo =
+  nil
+
+proc `$`(info: TThingInfo): string =
+  "JavaThing[type=$1, name=$2, static=$3]" % [$info.kind, info.name, $info.isStatic]
+
+proc parseJavaDecl(line: string): TThingInfo =
+  var rest = line[0..line.len - 2]
+  let isPublic = rest.maybeStripStart("public ")
+  let isProtected = rest.maybeStripStart("protected ")
+  let isStatic = rest.maybeStripStart("static ")
+  let isFinal = rest.maybeStripStart("final ")
+
+  var kind = javaMethod
+  var name: string
+  if not rest.stripFirstWord():
+    if rest != "{}":
+      kind = javaConstructor
+    else:
+      kind = javaSpecial
+    name = "<constructor>"
+  else:
+    let nameMatch = rest.matchLen(re"^\w+")
+    assert nameMatch != -1
+
+    name = rest[0..nameMatch-1]
+    rest = rest[nameMatch..rest.len-1]
+    if rest == "":
+      kind = javaField
+
+  return (name, kind, isPublic, isProtected, isStatic)
+
+proc parseSig(line): string =
+  string(line).split(':')[1].strip
+
+proc javap(name: string) =
+  echo "javap main"
+  let input = rawJavap(name)
+  var line: TaintedString = ""
+  # discard 'Compiled from "Foobar.java"'
+  discard input.readLine(line)
+  #
+  discard input.readLine(line)
+  let classInfo = parseClassInfo(line)
+  while input.readLine(line):
+    if line == "}":
+      break
+    # like: public boolean isEmpty();
+    let javaDecl = parseJavaDecl(line)
+    discard input.readLine(line)
+    # "  Signature: jnisig"
+    let sig = parseSig(line)
+    echo javaDecl, " ", sig
+
+when isMainModule:
+  javap "java.lang.String"
